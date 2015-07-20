1a215ba23c4cb86b3918e08e8a63f6df04afca28
Jérémy Pagé <contact@jeremypage.me>
2015-07-20 09:38:08 +0200

    Parser read from IO

--- lib/messages/parser.rb
+++ lib/messages/parser.rb
@@ -1,12 +1,22 @@
+require 'stringio'
+
 Dir[File.expand_path '*.rb', __dir__].each do |file|
   require_relative file
 end
 
 class OFParser
-  def self.read(binary)
-    type       = OFMessage::TYPES[binary[1].unpack('C')[0].to_i]
+  def self.read(io)
+    io = StringIO.new(io) if io.is_a?(String)
+
+    header = io.readpartial(OFMessage::HEADER_LENGTH)
+
+    type       = OFMessage::TYPES[header[1].unpack('C')[0].to_i]
     klass_name = 'OF' + type.to_s.split('_').map(&:capitalize).join
     klass      = Object.const_get(klass_name)
-    klass.read(binary)
+    length     = header[2..3].unpack('S>')[0].to_i
+
+    body = io.readpartial(length - OFMessage::HEADER_LENGTH)
+
+    klass.read(header + body)
   end
 end
--- spec/messages/parser_spec.rb
+++ spec/messages/parser_spec.rb
@@ -1,17 +1,9 @@
 require 'spec_helper'
 
 describe OFParser do
-  it 'should read binary Hello Message' do
-    msg = OFParser.read [1, 0, 0, 8, 0, 0, 0, 1].pack('C*')
-    expect(msg.class).to be(OFHello)
-    expect(msg.version).to eq(OFMessage::OFP_VERSION)
-    expect(msg.type).to eq(:hello)
-    expect(msg.len).to eq(8)
-    expect(msg.xid).to eq(1)
-  end
-
-  it 'should read binary PacketOut Message' do
-    msg = OFParser.read [
+  let(:hello_msg) { [1, 0, 0, 8, 0, 0, 0, 1].pack('C*') }
+  let(:packet_out_msg) {
+    [
       1, 13, 0, 26, 0, 0, 0, 1, # header
       0xff, 0xff, 0xff, 0xff,   # buffer_id
       0, 1,                     # in_port
@@ -24,6 +16,19 @@ describe OFParser do
       # data
       1, 2
     ].pack('C*')
+  }
+
+  it 'should read binary Hello Message' do
+    msg = OFParser.read(hello_msg)
+    expect(msg.class).to be(OFHello)
+    expect(msg.version).to eq(OFMessage::OFP_VERSION)
+    expect(msg.type).to eq(:hello)
+    expect(msg.len).to eq(8)
+    expect(msg.xid).to eq(1)
+  end
+
+  it 'should read binary PacketOut Message' do
+    msg = OFParser.read(packet_out_msg)
     expect(msg.class).to be(OFPacketOut)
     expect(msg.version).to eq(OFMessage::OFP_VERSION)
     expect(msg.type).to eq(:packet_out)
